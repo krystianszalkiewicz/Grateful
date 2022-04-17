9ba6326d8bc141879baf9dfa055f8577e9f4ca4d
Jaroslaw Gawlik <jargawl@gmail.com>
2022-04-18 00:07:56 +0100

    Implement Firebase to JanuaryPage

--- lib/features/MonthsPage/January/january.dart
+++ lib/features/MonthsPage/January/january.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -40,10 +41,70 @@ class JanuarygratefulPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'find resons to be grateful every day',
+          'Find resons to be grateful every day',
           style: GoogleFonts.pacifico(
             color: Colors.lightBlue,
+            fontSize: 20,
           ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          FirebaseFirestore.instance
+              .collection('january')
+              .add({'name': 'za wspaniałą rodzinę'});
+        },
+        child: const Icon(
+          Icons.add,
+          color: Color.fromARGB(255, 47, 184, 129),
+        ),
+      ),
+      body: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+          stream: FirebaseFirestore.instance.collection('january').snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return const Text('Something went wrong');
+            }
+
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            }
+            final documents = snapshot.data!.docs;
+            return ListView(
+              children: [
+                for (final document in documents) ...[
+                  NameWidget(document['name'])
+                ],
+              ],
+            );
+          }),
+    );
+  }
+}
+
+class NameWidget extends StatelessWidget {
+  const NameWidget(
+    this.name, {
+    Key? key,
+  }) : super(key: key);
+  final String name;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Color.fromARGB(255, 23, 213, 169),
+        borderRadius: BorderRadius.all(
+          Radius.circular(20),
+        ),
+      ),
+      padding: const EdgeInsets.all(15),
+      margin: const EdgeInsets.all(10),
+      child: Text(
+        (name),
+        style: GoogleFonts.pacifico(
+          color: Colors.white,
+          fontSize: 23,
         ),
       ),
     );
