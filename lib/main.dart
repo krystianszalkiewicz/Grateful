23e4289e2fc0f19ea226e624d11bfc6fac0fc002
Jaroslaw Gawlik <jargawl@gmail.com>
2022-04-30 09:38:28 +0200

    Organize files

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:thankfulness/firebase_options.dart';
+import 'App/app.dart';
 import 'firebase_options.dart';
 
-import 'features/HomePage /home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,18 +13,3 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Grateful',
-      
-      theme: ThemeData(
-        primarySwatch: Colors.yellow,
-      ),
-      home: const HomePage(),
-    );
-  }
-}
