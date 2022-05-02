c108685d06745620c2a113937b7e833107ee7d79
Jaroslaw Gawlik <jargawl@gmail.com>
2022-05-02 10:28:35 +0200

    Add SafeArea

--- lib/features/HomePage /home_page.dart
+++ lib/features/HomePage /home_page.dart
@@ -22,60 +22,63 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          actions: [
-            IconButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => const UserProfile(),
-                  ),
-                );
-              },
-              icon: const Icon(
-                Icons.person,
-              ),
-              color: const Color.fromARGB(255, 23, 213, 169),
-            )
-          ],
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.vertical(
-              bottom: Radius.circular(25),
-            ),
-          ),
-          title: Center(
-            child: Text(
-              'Be grateful everyday',
-              style: GoogleFonts.pacifico(
-                color: const Color.fromARGB(255, 23, 213, 169),
-                fontSize: 26,
-              ),
-            ),
-          ),
-        ),
-        backgroundColor: Color.fromARGB(255, 216, 228, 237),
-        drawer: Drawer(
-          child: ListView(
-            children: const [
-              January(),
-              February(),
-              March(),
-              April(),
-              May(),
-              June(),
-              July(),
-              August(),
-              September(),
-              October(),
-              November(),
-              December(),
+    return SafeArea(
+      child: Scaffold(
+          appBar: AppBar(
+            actions: [
+              IconButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => const UserProfile(),
+                    ),
+                  );
+                },
+                icon: const Icon(
+                  Icons.person,
+                ),
+                color: const Color.fromARGB(255, 0, 0, 0),
+                iconSize: 35,
+              )
             ],
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(
+                bottom: Radius.circular(25),
+              ),
+            ),
+            title: Center(
+              child: Text(
+                'Be grateful everyday',
+                style: GoogleFonts.pacifico(
+                  color: const Color.fromARGB(255, 23, 213, 169),
+                  fontSize: 26,
+                ),
+              ),
+            ),
           ),
-        ),
-        body: const RiveAnimation.asset(
-          'assets/a.riv',
-          alignment: Alignment.bottomCenter,
-        ));
+          backgroundColor: const Color.fromARGB(255, 216, 228, 237),
+          drawer: Drawer(
+            child: ListView(
+              children: const [
+                January(),
+                February(),
+                March(),
+                April(),
+                May(),
+                June(),
+                July(),
+                August(),
+                September(),
+                October(),
+                November(),
+                December(),
+              ],
+            ),
+          ),
+          body: const RiveAnimation.asset(
+            'assets/a.riv',
+            alignment: Alignment.bottomCenter,
+          )),
+    );
   }
 }
